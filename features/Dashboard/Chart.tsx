7ae49a5577fae8512876907262e88fae20c15647
Obijuru <158084677+Obijuru@users.noreply.github.com>
2025-07-06 02:31:49 +0100

    arrrange the design

--- features/Dashboard/Chart.tsx
+++ features/Dashboard/Chart.tsx
@@ -114,52 +114,37 @@ export const Chart = () => {
     };
 
   return (
-    <div className="">
+    <div className="bg-gray-100">
       <div className="flex justify-between">
         <h2 className="text-[#272424] p-2 font-bold text-lg rounded-md">Welcome Onboard, {user?.name}</h2>
         {/* <Header title={getHeaderTitle() || "Header"} activeTab={activeTab} setActiveTab={setActiveTab} /> */}
       </div>
       
 
-      <div className="grid md:grid-cols-2 gap-10 px-5">
-        <div className="flex flex-col gap-4 m-auto">
-          <div className="flex">
-            <div className="bg-[#4d1b7a] flex flex-col w-1/2 px-5 py-3 rounded-lg h-45 gap-6">
-              <h2 className="text-sm text-[#C2C2C2]">Available Balance</h2>
-              <h2 className="text-white text-sm font-semibold">$ 321,500.09</h2>
-              <div className='flex justify-between mt-6'>
-                <h6 className='text-white text-sm'>****4866</h6>
-                <Image
-                  src='/mastercard.png'
-                  alt="mastercardlogo"
-                  width={20}
-                  height={20}
-                  className='rounded-full'
-                />
-              </div>
+      {/* <div className="gap-10 px-5"> */}
+        <div className="flex justify-center items-center">
+          {/* <div className="bg-[#4d1b7a] flex flex-col px-10 py-7 rounded-lg gap-6">
+            <h2 className="text-sm text-[#C2C2C2]">Available Balance</h2>
+            <h2 className="text-white text-sm font-semibold">$ 321,500.09</h2>
+            <div className='flex justify-between mt-6'>
+              <h6 className='text-white text-sm'>****4866</h6>
+              <Image
+                src='/mastercard.png'
+                alt="mastercardlogo"
+                width={20}
+                height={20}
+                className='rounded-full'
+              />
             </div>
-            <div className="flex flex-col justify-center gap-4 px-5">
-              <h4 className="text-sm">Monthly Income</h4>
-              {/* <h4 className="text-[#4d1b7a] font-bold">$ 500,000</h4> */}
-              <h2 className="text-[#4d1b7a] font-bold">
-               $ {budget
-                    ? 'error' in budget
-                      ? budget.error
-                      : (budget as Budget).amount
-                    : <BeatLoader size={5}/> 
-                  }
-              </h2>
-          </div>
-          </div>
-  
-          <div className="flex gap-10">
+          </div> */}
+          <div className="flex gap-2 md:gap-10 md:px-0 px-20">
+            
             <div className="flex flex-col items-center gap-4 bg-[#e2dfdf] px-10 py-7 rounded-lg">
               <div className="flex gap-5">
                 <h4 className="text-sm">Total Income</h4>
                 <ArrowDownIcon className="text-[#3aac2e]"/>
               </div>
-              {/* <h2 className="text-center text-[#175d27]  font-bold">$ 350,000</h2> */}
-              <h2 className="text-center text-[#175d27]  font-bold">
+              <h2 className="text-center text-[#175d27] font-bold">
                 $ {total
                   ? 'error' in total
                     ? total.error
@@ -167,6 +152,19 @@ export const Chart = () => {
                   : <BeatLoader size={5} />}
               </h2>
             </div>
+
+            <div className="flex flex-col items-center gap-4 bg-[#e2dfdf] px-10 py-7 rounded-lg">
+              <h4 className="text-sm">Monthly Income</h4>
+              <h2 className="text-[#4d1b7a] font-bold text-sm">
+                $ {budget
+                    ? 'error' in budget
+                      ? budget.error
+                      : (budget as Budget).amount
+                    : <BeatLoader size={5}/>
+                  }
+              </h2>
+            </div>
+
             <div className="flex flex-col items-center  gap-4 bg-[#e2dfdf] px-10 py-7 rounded-lg">
               <div className="flex gap-5">
                 <h4 className="text-sm">Total Expense</h4>
@@ -175,29 +173,35 @@ export const Chart = () => {
                 <h2 className="text-center text-[#8a0b0b] font-bold">$ 170,000</h2>
             </div>
           </div>
+
         </div>
 
-        <ExpenseChart />
+
+        <div className="flex flex-col md:flex-row md:mt-20">
+
+          <ExpenseChart />
 
 
-        <div className="px-10">
-          <h2 className="text-lg font-semibold text-[#863cc6]">Recent Activities</h2>
-          <div className="border"></div>
-          {expense && Array.isArray(expense) ? expense.slice(0,4).map((item, idx) => (
-            <div className="flex justify-between mt-4" key={idx}>
-              <div className="flex justify-center items-center gap-6">
-                <span className="inline-block w-3 h-3 bg-red-500 rounded-full"></span>
-                <div>
-                  <h3 className="font-semibold text-sm">{item.item}</h3>
-                  <h5 className="text-sm text-start">{formatInTimeZone(new Date(item.createdAt), 'UTC', 'yyyy-MM-dd')}</h5>
+          <div className="px-10 mt-10 md:mt-0 md:w-1/2">
+            <h2 className="text-lg font-semibold text-[#863cc6]">Recent Activities</h2>
+            <div className="border"></div>
+            {expense && Array.isArray(expense) ? expense.slice(0,4).map((item, idx) => (
+              <div className="flex justify-between mt-4" key={idx}>
+                <div className="flex justify-center items-center gap-6">
+                  <span className="inline-block w-3 h-3 bg-red-500 rounded-full"></span>
+                  <div>
+                    <h3 className="font-semibold text-sm">{item.item}</h3>
+                    <h5 className="text-sm text-start">{formatInTimeZone(new Date(item.createdAt), 'UTC', 'yyyy-MM-dd')}</h5>
+                  </div>
                 </div>
+                <h3 className="text-[#863cc6]">{item.amount}</h3>
               </div>
-              <h3 className="text-[#863cc6]">{item.amount}</h3>
-            </div>
-          )): <div className="text-center"><BeatLoader size={5}/></div>}
+            )): <div className="text-center"><BeatLoader size={5}/></div>}
+          </div>
+
         </div>
 
-        <div>
+        {/* <div>
           <div className="flex justify-between mb-5 px-5">
             <h2 className="text-[#7a33b9] font-semibold">Monthly Expenses</h2>
             <button className="text-[#7a33b9]">Edit</button>
@@ -210,9 +214,9 @@ export const Chart = () => {
               </div>
             ))}
           </div>
-        </div>
+        </div> */}
 
-      </div>
+      {/* </div> */}
     </div>
   )
 }
